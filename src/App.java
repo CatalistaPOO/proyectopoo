ec8d8d386ae47bef62c8de357643b10427e05b49
CatalistaPOO <fgtpoo78@gmail.com>
2025-06-13 11:17:21 +0200

    Exceptions

--- src/App.java
+++ src/App.java
@@ -5,7 +5,9 @@ import com.objetos.Persona;
 
 //las clases siempre seran con mayúsculas (App)
 public class App {
-    public static void main(String[] args) throws Exception {
+    //Esta linea contiene throws Exception y la eliminamos (para usar try/catch)
+    // public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Usando App");
         System.out.println("-------------------------------------------");
         //EJECUTAMOS UN METODO DE OTRA CLASE (PRUEBA)
@@ -16,10 +18,21 @@ public class App {
 
         System.out.println("-------------------------------------------");
 
-        personaje.nombre = "Lucas";
-        personaje.apellidos = "Lopez";
-        personaje.edad = 30;
+        personaje.setNombre("Lucas"); 
+        personaje.setApellido("Lopez");
+        //setEdad de la clase PERSONA, en su método contiene throws Exception y Exception en particular si es negativo
+        //por esto el método debe ir con try/catch (intenta setearlo y lanza error en la exception contemplada)
+        try {
+            personaje.setEdad(30); 
+        } catch (Exception e) {
+            // Un catch nunca estará vacío
+            System.out.println("ERROR: " + e.getMessage());
+        } finally{
+            //finally siempre se ejecuta con indepenedencia de si hay Exception o no. Es opcional.
+            //Sirve para asegurar que se cumple un paso sí o sí a pesar del Exception.
+            //ejemplo una conexion a BBDD y si pasa algo por el camino, la conexion siempre la cerraremos en finally
+        }
 
-        System.out.println("OBJETO PERSONAJE(creado con la clase Persona.java): \nNombre y apellido: " + personaje.nombre + " " + personaje.apellidos + "\nEdad: " +  personaje.edad);
+        System.out.println("OBJETO PERSONAJE(creado con la clase Persona.java): \nNombre y apellido: " + personaje.getNombre() + " " + personaje.getApellido() + "\nEdad: " +  personaje.getEdad());
     }
 }
--- src/com/objetos/Persona.java
+++ src/com/objetos/Persona.java
@@ -1,7 +1,39 @@
 package com.objetos;
 
 public class Persona {
-    public String nombre;
-    public String apellidos;
-    public int edad;
+    // Las variables de Persona, serán privadas:
+    private String nombre;
+    private String  apellido;
+    private int edad;
+    //podemos generar los Getter  y Setter con CodeGenerator (extensionVsCode)
+    //click derecho y codegenerator for Java/generate getter and setters
+    
+  
+    // Por cada propiedad habra metodo Get (recuperar) y set opcionalmente(dar valor):
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String nombre){//setter llama a su valor con el nombre de su propiedad
+        this.nombre = nombre;//LA VARIABLE STRING TIENE EL MISMO NOMBRE QUE LA VARIABLE QUE RECIBE;
+    }
+    
+    public String getApellido(){
+        return this.apellido;
+    }
+    public void setApellido(String apellido){
+        this.apellido = apellido;//LA VARIABLE STRING TIENE EL MISMO NOMBRE QUE LA VARIABLE QUE RECIBE;
+    }
+    
+    public int getEdad(){
+            return this.edad;
+        }
+    public void setEdad(int edad)throws Exception{//El metodo puede dar un error si edad = 0, anunciamos posible error con throws Exception
+        if (edad < 0){
+            throw new Exception("La edad no puede ser negativa");//anunciamos el motivo de error con nombre
+        }
+        else{
+            //this se refiere a edad en la clase definida por primera vez
+            this.edad = edad;//LA VARIABLE INT TIENE EL MISMO NOMBRE QUE LA VARIABLE QUE RECIBE;
+        }
+    }
 }
